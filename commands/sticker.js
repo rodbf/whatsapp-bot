c9d05df5bd1e887a8f2ca0e504dbc32d5f2d7a6e
Rodrigo Bueno Ferreira <ferreirarodrigobueno@gmail.com>
2022-01-10 20:17:49 -0300

    readme

--- commands/sticker.js
+++ commands/sticker.js
@@ -3,9 +3,13 @@ module.change_code = 1;
 module.exports = {
 	description:"envie uma imagem com esse comando para transormar em um sticker",
 	process: async (msg, callback) => {
-		if(msg.type == 'image'){
+		console.log(msg);
+		if(msg.hasMedia){
+			console.log("hasMedia");
 			const media = await msg.downloadMedia();
+			console.log("checkpoint 1");
 			msg.reply(media, msg.getChat().id, {sendMediaAsSticker: true});
+			console.log("checkpoint 2");
 			return;
 		}
 		msg.reply("Envie uma imagem com esse comando.");
